7e5e22518c2c1c699344e58171af232053b4c682
dhanubor <nikhilmistry121203@gmail.com>
2025-02-07 07:46:38 +0600

    g

---
+++ 006_js_operator/app.js
@@ -0,0 +1,13 @@
+// let userInput = prompt('Enter a Number');
+// let number = parseInt(userInput);
+
+// if (number % 2 === 0){
+//     console.log(`${number} is even number`);
+// }else{
+//     console.log(`${number} is odd number`);
+// }
+let x = 10;
+x++;
+console.log(x);
+const name =parseInt('2')
+console.log(name);
---
+++ oparator.js
@@ -0,0 +1,26 @@
+
+let x= 10, y=20;
+
+console.log(x+y);
+let c=23, d=5;
+let divition= c%d
+console.log(divition);
+
+let p= 3, q = 4;
+let square = p**q;
+console.log(square)
+//incrice
+let number = 10;
+// number ++;
+number --;
+console.log(number);
+
+let a= 9;
+a++;
+console.log(a);
+++a;
+console.log(a);
+a--;
+console.log(a);
+--a;
+console.log(a);
